28621d9331af788f795d1a877c8d316b2a669c9a
cch-robo <robo@cch-lab.com>
2025-08-01 00:28:38 +0900

    コマンドラインオプション指定のテストを追加

--- test/grep_library_test.dart
+++ test/grep_library_test.dart
@@ -1,8 +1,31 @@
-import 'package:grep_library/grep_cli.dart';
+import 'package:grep_library/src/cli_foundation/cli_options_parser.dart';
+import 'package:grep_library/src/cli_foundation/errors/file_not_exist_exception.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('no_options', () {
+    // ファイルパス指定なし
+    expect(
+      () => createOptionsParameters(getOptionsParser(), <String>[]),
+      throwsA(isA<FileNotExistException>()),
+    );
+  });
+
+  test('not_exist_option', () {
+    // 存在しないファイルへのファイルパス
+    expect(
+      () => createOptionsParameters(getOptionsParser(), ['dummy.txt']),
+      throwsA(isA<FileNotExistException>()),
+    );
+  });
+
+  test('exist_option', () {
+    // 存在するファイルへのファイルパス
+    expect(
+      createOptionsParameters(getOptionsParser(), [
+        'bin/grep_cli.dart',
+      ]).paths.first.path,
+      'bin/grep_cli.dart',
+    );
   });
 }
