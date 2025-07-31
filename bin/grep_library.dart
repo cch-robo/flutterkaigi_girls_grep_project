a6ceea9d4b6db54ba061f996ea22d414071d5b69
cch-robo <robo@cch-lab.com>
2025-07-31 15:26:24 +0900

    first commit

---
+++ bin/grep_library.dart
@@ -0,0 +1,5 @@
+import 'package:grep_library/grep_library.dart' as grep_library;
+
+void main(List<String> arguments) {
+  print('Hello world: ${grep_library.calculate()}!');
+}
---
+++ test/grep_library_test.dart
@@ -0,0 +1,8 @@
+import 'package:grep_library/grep_library.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
