175266873c0db35ee88ebdbafb393e30e41ebab5
cch-robo <robo@cch-lab.com>
2025-07-31 15:48:01 +0900

    grep コマンドライン・アプリ用の構成変更に伴う修正

--- bin/grep_cli.dart
+++ bin/grep_cli.dart
@@ -1,4 +1,4 @@
-import 'package:grep_library/grep_library.dart' as grep_library;
+import 'package:grep_library/grep_cli.dart' as grep_library;
 
 void main(List<String> arguments) {
   print('Hello world: ${grep_library.calculate()}!');
--- test/grep_library_test.dart
+++ test/grep_library_test.dart
@@ -1,4 +1,4 @@
-import 'package:grep_library/grep_library.dart';
+import 'package:grep_library/grep_cli.dart';
 import 'package:test/test.dart';
 
 void main() {
