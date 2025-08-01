db84057294314b92c6c2afbb9a4fdee3f5a13042
cch-robo <robo@cch-lab.com>
2025-08-01 15:08:48 +0900

    パッケージ名変名により削除

--- lib/src/cli_foundation/errors/file_not_exist_exception.dart
+++
@@ -1,6 +0,0 @@
-import '../../api_foundation/abstract_exception.dart';
-
-class FileNotExistException extends AbstractException {
-  FileNotExistException({required String filePath})
-    : super(message: 'File or Directory not exist. - filePath=$filePath');
-}
