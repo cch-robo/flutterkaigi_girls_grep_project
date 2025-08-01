982c7734e83f7e2840bd11fe1b35fcbde271cc2f
cch-robo <robo@cch-lab.com>
2025-08-01 19:10:25 +0900

    launchCliAppを非同期処理に修正。

--- bin/grep_cli.dart
+++ bin/grep_cli.dart
@@ -1,5 +1,5 @@
 import 'package:grep_library/src/cli_foundation/cli_app_launcher.dart';
 
-void main(List<String> arguments) {
-  launchCliApp(arguments);
+void main(List<String> arguments) async {
+  await launchCliApp(arguments);
 }
