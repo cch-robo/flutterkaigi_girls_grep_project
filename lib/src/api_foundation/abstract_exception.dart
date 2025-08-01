26ee59dfef4d358a90c6451837e5042f585be1b6
cch-robo <robo@cch-lab.com>
2025-08-01 13:23:21 +0900

    プライベート変数の位置を修正

--- lib/src/api_foundation/abstract_exception.dart
+++ lib/src/api_foundation/abstract_exception.dart
@@ -2,10 +2,10 @@ abstract class AbstractException implements Exception {
   AbstractException({this.message, StackTrace? stackTrace})
     : _stackTrace = stackTrace ?? StackTrace.current;
 
+  final StackTrace _stackTrace;
+
   /// メッセージ
   final String? message;
-
-  final StackTrace _stackTrace;
 
   /// スタックトレース
   StackTrace get stackTrace => _stackTrace;
