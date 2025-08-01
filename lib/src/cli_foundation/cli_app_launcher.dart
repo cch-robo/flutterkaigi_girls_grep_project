a069f432578a4d4cd888aeff72667c9152157899
cch-robo <robo@cch-lab.com>
2025-08-01 09:41:39 +0900

    パーサ生成とパラメータ生成の分離に伴う修正

--- lib/src/cli_foundation/cli_app_launcher.dart
+++ lib/src/cli_foundation/cli_app_launcher.dart
@@ -1,29 +1,29 @@
 import 'dart:io';
 
-import 'package:args/args.dart';
 import 'package:grep_library/src/api_foundation/abstract_exception.dart';
 import 'package:grep_library/src/cli_foundation/cli_message.dart';
+import 'package:grep_library/src/cli_foundation/cli_options_parameter.dart';
 import 'package:grep_library/src/cli_foundation/cli_options_parser.dart';
 import 'package:grep_library/src/cli_foundation/cli_parameter_model.dart';
 
 void launchCliApp(List<String> arguments) {
   // TODO パラメータ・チェックが終われば削除すること。
-  ArgResults argResults = getOptionsParserResults(arguments, getOptionsParser());
-  argResults.options.forEach((String key) => print('options key=$key'));
-  print('help=${argResults['help']}');
-  print('regexp=${argResults['regexp']}');
-  print('rest=${argResults.rest}');
+  CliOptionParser parser = CliOptionParser(arguments: arguments);
+  parser.argResults.options.forEach((String key) => print('options key=$key'));
+  print('help=${parser.argResults['help']}');
+  print('regexp=${parser.argResults['regexp']}');
+  print('rest=${parser.argResults.rest}');
 
-  bool hasUsage = hasOptionsUsage(getOptionsParserResults(arguments, getOptionsParser()));
+  bool hasUsage = parser.hasOptionsUsage;
   if (hasUsage) {
     CliMessage help = CliMessage(isUseColor: true);
-    help.putMessage(getOptionsUsage(getOptionsParser()));
+    help.putMessage(parser.optionsUsage);
     exit(0);
   }
 
   CliMessage errorMessage = CliMessage();
   try {
-    CliParameter param = createOptionsParameters(getOptionsParser(), arguments);
+    CliParameter param = createOptionsParameter(parser.argResults);
 
     // TODO パラメータを使った処理を追加する。（現状は、コマンドライン・パラメータ取得確認のみ）
     CliMessage mes = CliMessage(isUseColor: true);
--- test/grep_library_test.dart
+++ test/grep_library_test.dart
@@ -1,3 +1,4 @@
+import 'package:grep_library/src/cli_foundation/cli_options_parameter.dart';
 import 'package:grep_library/src/cli_foundation/cli_options_parser.dart';
 import 'package:grep_library/src/cli_foundation/errors/file_not_exist_exception.dart';
 import 'package:test/test.dart';
@@ -5,26 +6,31 @@ import 'package:test/test.dart';
 void main() {
   test('no_options', () {
     // ファイルパス指定なし
+    final CliOptionParser parser = CliOptionParser(arguments: <String>[]);
     expect(
-      () => createOptionsParameters(getOptionsParser(), <String>[]),
+      () => createOptionsParameter(parser.argResults),
       throwsA(isA<FileNotExistException>()),
     );
   });
 
   test('not_exist_option', () {
     // 存在しないファイルへのファイルパス
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['dummy.txt'],
+    );
     expect(
-      () => createOptionsParameters(getOptionsParser(), ['dummy.txt']),
+      () => createOptionsParameter(parser.argResults),
       throwsA(isA<FileNotExistException>()),
     );
   });
 
   test('exist_option', () {
     // 存在するファイルへのファイルパス
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['bin/grep_cli.dart'],
+    );
     expect(
-      createOptionsParameters(getOptionsParser(), [
-        'bin/grep_cli.dart',
-      ]).paths.first.path,
+      createOptionsParameter(parser.argResults).paths.first.path,
       'bin/grep_cli.dart',
     );
   });
