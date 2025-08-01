1e9904ecb49297dc7c51cf6c1f47d18864047e6d
cch-robo <robo@cch-lab.com>
2025-08-01 13:22:37 +0900

    スタックトレースを追加

--- lib/src/api_foundation/abstract_exception.dart
+++ lib/src/api_foundation/abstract_exception.dart
@@ -1,6 +1,12 @@
 abstract class AbstractException implements Exception {
-  AbstractException({this.message});
+  AbstractException({this.message, StackTrace? stackTrace})
+    : _stackTrace = stackTrace ?? StackTrace.current;
 
   /// メッセージ
   final String? message;
+
+  final StackTrace _stackTrace;
+
+  /// スタックトレース
+  StackTrace get stackTrace => _stackTrace;
 }
