a9d6e625ceb862507b5400cb55ca4cd4a3d4e989
cch-robo <robo@cch-lab.com>
2025-08-01 13:48:04 +0900

    エラーチェック大幅改訂に伴うテスト修正

--- test/grep_library_test.dart
+++ test/grep_library_test.dart
@@ -1,33 +1,87 @@
 import 'package:grep_library/src/cli_foundation/cli_options_parameter.dart';
 import 'package:grep_library/src/cli_foundation/cli_options_parser.dart';
-import 'package:grep_library/src/cli_foundation/errors/file_not_exist_exception.dart';
+import 'package:grep_library/src/cli_foundation/errors/can_not_find_files_exception.dart';
+import 'package:grep_library/src/cli_foundation/errors/invalid_path_exception.dart';
+import 'package:grep_library/src/cli_foundation/errors/pattern_not_exist_exception.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('no_options', () {
-    // ファイルパス指定なし
+    // PATTERNS もファイルパスも指定なし
     final CliOptionParser parser = CliOptionParser(arguments: <String>[]);
     expect(
       () => createOptionsParameter(parser.argResults),
-      throwsA(isA<FileNotExistException>()),
+      throwsA(isA<PatternNotExistException>()),
     );
   });
 
-  test('not_exist_option', () {
-    // 存在しないファイルへのファイルパス
+  test('no_patterns_option', () {
+    // PATTERNS にファイルパスが指定されている。
     final CliOptionParser parser = CliOptionParser(
-      arguments: <String>['dummy.txt'],
+      arguments: <String>['bin/grep_cli.dart'],
     );
     expect(
       () => createOptionsParameter(parser.argResults),
-      throwsA(isA<FileNotExistException>()),
+      throwsA(isA<PatternNotExistException>()),
     );
   });
 
-  test('exist_option', () {
-    // 存在するファイルへのファイルパス
+  test('only_exist_patterns_option', () {
+    // PATTERNS のみが指定されている。
     final CliOptionParser parser = CliOptionParser(
-      arguments: <String>['bin/grep_cli.dart'],
+      arguments: <String>['hello world!'],
+    );
+    expect(
+      createOptionsParameter(parser.argResults).regexps.first.pattern,
+      'hello world!',
+    );
+  });
+
+  test('no_filepath_option', () {
+    // PATTERNS があるが、ファイルパスのファイルが存在しない。
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['hello world!', 'good morning!'],
+    );
+    expect(
+          () => createOptionsParameter(parser.argResults),
+      throwsA(isA<InvalidPathException>()),
+    );
+  });
+
+  test('not_exist_filepath_option', () {
+    // PATTERNS もファイルパスもあるが、ファイルパスのファイルが存在しない。
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['hello world!', 'dummy.txt'],
+    );
+    expect(
+      () => createOptionsParameter(parser.argResults),
+      throwsA(isA<InvalidPathException>()),
+    );
+  });
+
+  test('no_recursive_directory_option', () {
+    // PATTERNS もファイルパスのディレクトリも存在するが、再帰探索フラグがない。
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['hello world!', 'bin'],
+    );
+    expect(
+      () => createOptionsParameter(parser.argResults),
+      throwsA(isA<CanNotFindFilesException>()),
+    );
+  });
+
+  test('exist_recursive_directory_option', () {
+    // PATTERNS もファイルパスのディレクトリも存在し、再帰探索フラグも存在する。
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['-r', 'hello world!', 'bin'],
+    );
+    expect(createOptionsParameter(parser.argResults).paths.first.path, 'bin');
+  });
+
+  test('exist_both_option', () {
+    // PATTERNS も存在するファイルへのファイルパスもある。
+    final CliOptionParser parser = CliOptionParser(
+      arguments: <String>['hello world!', 'bin/grep_cli.dart'],
     );
     expect(
       createOptionsParameter(parser.argResults).paths.first.path,
